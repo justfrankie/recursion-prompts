e52b84704bebf4713131b3c7f0333ca9174f8388
Frankie Liu <zhuangfrankie@gmail.com>
2020-01-24 23:15:54 -0800

    completed factorial

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,21 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+    
+    if (n < 0) {
+        return null
+    } else if (n <= 1) { // base case
+        return 1
+    }
+    return n * factorial(n - 1)// recursive function
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    //base case (end)
+    return sum(array.length[])// recursive case calling itself
+
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
